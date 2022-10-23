40db22f7e1bf952a6ba6f48b3f2fdfe4c86a98f7
sheshankpriyadarshi <sheshankpriyadarshi@gmail.com>
2022-10-23 20:37:34 +0530

    updated

---
+++ vars/genericBuild.groovy
@@ -0,0 +1,34 @@
+node {
+  stage('SCM') 
+    {
+        echo 'Gathering code from version control'
+        git branch: '${branch}', url: 'https://github.com/sheshankpriyadarshi/jenkins.git'
+    }
+    stage('Build') 
+    {
+      try{
+        bat 'dotnet --version' 
+        bat 'dotnet build ' + config.target
+        echo 'Building new feature...'
+        releasenotes(changes:"true")
+      }
+      catch(e)
+      {
+        echo 'something is not right'
+        echo e.toString();
+        currentBuild.result = "FAILURE"
+      }
+      finally
+      {
+        //cleanup
+      }
+    }
+      stage('Test') 
+    {
+        echo 'Testing...'
+    }
+      stage('Deploy') 
+    {
+        echo 'Deploying...'
+    }
+}
